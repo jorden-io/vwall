1bcc80e1d8bf73764d3d6ea9255e4df521931a0b
Jorden Rodriguez <jorden@Jordens-Mac-mini.local>
2024-06-10 10:45:01 -0500

    changed routes

--- vwall/src/App.tsx
+++ vwall/src/App.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     const body = { data };
-    await fetch("http://18.191.156.238:8000/message", {
+    await fetch("https://vi2bi0yw08.execute-api.us-east-2.amazonaws.com/prod/message", {
       method: "POST",
       headers: myHeaders,
       body: JSON.stringify(body),
@@ -16,50 +16,50 @@ export default function Home() {
   };
   useEffect(() => {
     (async () => {
-      const data = await fetch("http://18.191.156.238:8000/");
+      const data = await fetch("https://vi2bi0yw08.execute-api.us-east-2.amazonaws.com/prod/all");
       const r = await data.json();
       setState(r);
       console.log(r);
     })();
   }, []);
   return (
-      <div style={{}}>
-        <h1
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            fontSize: "20px",
-            fontWeight: "100",
-            margin: "20px",
-          }}
-        >
-          welcome, dont say anything dumb
-        </h1>
-        <div
-          style={{
-            backgroundColor: "rgb(45 45 45)",
-            boxShadow: "inset 0px 0px 8px black",
-            border: "solid 1px lightseagreen",
-            height: "500px",
-            margin: "30px",
-            overflowY: "scroll",
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "column",
-          }}
-        >
-          {state.map((e: any) => (
-            <div key={e.mid}>
-              <p key={e!.mid!} style={{ padding: "10px", fontWeight: "150" }}>
-                {e!.mid!} - {e!.time[5]}
-                {e!.time[6]} / {e!.time[8]}
-                {e!.time[9]} - {e!.data!}
-              </p>
-              <hr style={{ border: "solid 1px grey" }}></hr>
-            </div>
-          ))}
-        </div>
-        <div style={{display: "flex", justifyContent: "center"}}>
+    <div style={{}}>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "20px",
+          fontWeight: "100",
+          margin: "20px",
+        }}
+      >
+        welcome, dont say anything dumb
+      </h1>
+      <div
+        style={{
+          backgroundColor: "rgb(45 45 45)",
+          boxShadow: "inset 0px 0px 8px black",
+          border: "solid 1px lightseagreen",
+          height: "500px",
+          margin: "30px",
+          overflowY: "scroll",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {state.map((e: any) => (
+          <div key={e.mid}>
+            <p key={e!.mid!} style={{ padding: "10px", fontWeight: "150" }}>
+              {e!.mid!} - {e!.time[5]}
+              {e!.time[6]} / {e!.time[8]}
+              {e!.time[9]} - {e!.data!}
+            </p>
+            <hr style={{ border: "solid 1px grey" }}></hr>
+          </div>
+        ))}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <div
           style={{
             display: "flex",
@@ -87,7 +87,7 @@ export default function Home() {
             send
           </button>
         </div>
-        </div>
       </div>
+    </div>
   );
 }
